4f0fb97cfcc340d844f397f3140e46b81d689b9e
Kevin Ibay <kevinibaycs@gmail.com>
2024-04-18 11:57:41 -0500

    add Guests table

--- HotelAppDB/dbo/Tables/Rooms.sql
+++ HotelAppDB/dbo/Tables/Rooms.sql
@@ -3,5 +3,5 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [RoomNumber] VARCHAR(10) NOT NULL, 
     [RoomTypeId] INT NOT NULL, 
-    CONSTRAINT [FK_Rooms_RoomTypes] FOREIGN KEY (RoomTypeId) REFERENCES RoomTypes(Id)
+    CONSTRAINT [FK_Rooms_RoomTypes] FOREIGN KEY ([RoomTypeId]) REFERENCES RoomTypes(Id)
 )

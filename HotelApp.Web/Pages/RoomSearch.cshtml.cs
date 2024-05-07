ae8391d78bd8ea03517b53afa928d746ae2a6d63
Kevin Ibay <kevinibaycs@gmail.com>
2024-05-06 20:08:09 -0500

    add room search UI

---
+++ HotelApp.Web/Pages/RoomSearch.cshtml.cs
@@ -0,0 +1,47 @@
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelApp.Web.Pages
+{
+    public class RoomSearchModel : PageModel
+    {
+        private readonly IDatabaseData _db;
+
+        [DataType(DataType.Date)]
+        [BindProperty(SupportsGet = true)]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        [DataType(DataType.Date)]
+        [BindProperty(SupportsGet = true)]
+        public DateTime EndDate { get; set; } = DateTime.Now.AddDays(1);
+
+        [BindProperty(SupportsGet = true)]
+        public bool SearchSubmitted { get; set; } = false;
+
+        public List<RoomTypeModel> AvailableRoomTypes { get; set; }
+
+        public RoomSearchModel(IDatabaseData db)
+        {
+            _db = db;
+        }
+
+
+        public void OnGet()
+        {
+            if(SearchSubmitted == true)
+            {
+                AvailableRoomTypes = _db.GetAvailableRoomTypes(StartDate, EndDate);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage(new { SearchSubmitted = true, StartDate, EndDate});
+        }
+    }
+}
